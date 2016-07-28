ee45b16ab7ceb7d3622c17233ec963cea0cd6eb3
Inderpreet Pabla <inderpreet.pabla@gmail.com>
2016-07-27 22:16:09 -0400

    Basic number recognition complete

--- ImageAnalysisDataCreator/src/DataCreator2.java
+++ ImageAnalysisDataCreator/src/DataCreator2.java
@@ -6,8 +6,14 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,14 +25,25 @@ import javax.swing.JLabel;
 import java.awt.SystemColor;
 import java.awt.Font;
 
+
 public class DataCreator2 extends JPanel implements MouseMotionListener, MouseListener{
 	private boolean mouseDown = false;
 	private float mouseX = 0f;
 	private float mouseY = 0f;
-	private int[][] data = new int[25][25];
-	
-	int testSet = 9;
+	private int[][] data = new int[15][15];
+	private int[] info = new int[10];
+	int testSet = 70;
 	int setItem = 0;
+	
+	Socket socket;
+	InetAddress ip;
+	OutputStream outToServer;
+	DataOutputStream out;
+	
+	InputStream inToServer;
+	DataInputStream in;
+	
+	byte[] writeData = new byte[15*15];
     public DataCreator2() {
         setBackground(SystemColor.activeCaption);
         setLayout(null);
@@ -34,13 +51,13 @@ public class DataCreator2 extends JPanel implements MouseMotionListener, MouseLi
         JButton clearButton = new JButton("Clear");
         clearButton.setFont(new Font("Tahoma", Font.BOLD, 11));
         clearButton.setForeground(Color.RED);
-        clearButton.setBounds(10, 306, 89, 23);
+        clearButton.setBounds(10, 372, 61, 23);
         add(clearButton);
         
         clearButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				
-				data = new int[25][25];
+				data = new int[15][15];
 			}
 		});
         
@@ -50,16 +67,45 @@ public class DataCreator2 extends JPanel implements MouseMotionListener, MouseLi
         JButton saveButton = new JButton("Save");
         saveButton.setFont(new Font("Tahoma", Font.BOLD, 11));
         saveButton.setForeground(Color.RED);
-        saveButton.setBounds(151, 306, 89, 23);
+        saveButton.setBounds(81, 372, 61, 23);
         add(saveButton);
+        
+        JButton startConnectionButton = new JButton("Connect");
+        startConnectionButton.setForeground(Color.RED);
+        startConnectionButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+        startConnectionButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		 try{
+
+        	        	ip = InetAddress.getLocalHost();
+        	        	socket = new Socket(ip,12345);
+        	        	
+        	        	outToServer = socket.getOutputStream();
+        	            out = new DataOutputStream(outToServer);
+        	        	
+        	            inToServer = socket.getInputStream();
+        	            in = new DataInputStream(inToServer);
+        	            
+        	            Thread mainThread = new Thread(mainRunnable);
+        	    		mainThread.start(); // Start the main thread.
+        	            
+        	        }
+        	        catch(Exception error){System.out.println(error);}
+        	}
+        });
+        startConnectionButton.setBounds(152, 372, 86, 23);
+        add(startConnectionButton);
+        
+        
+        
         
         saveButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				
 				String path = testSet+"_"+setItem+".png";
-			    BufferedImage image = new BufferedImage(25, 25, BufferedImage.TYPE_INT_RGB);
-			    for (int x = 0; x < 25; x++) {
-			        for (int y = 0; y < 25; y++) {
+			    BufferedImage image = new BufferedImage(15, 15, BufferedImage.TYPE_INT_RGB);
+			    for (int x = 0; x < 15; x++) {
+			        for (int y = 0; y < 15; y++) {
 			        	Color c = Color.white;
 			        	if(data[x][y]==1)
 			        		c = Color.black;
@@ -79,7 +125,20 @@ public class DataCreator2 extends JPanel implements MouseMotionListener, MouseLi
 			    	testSet++;
 			    	setItem = 0;
 			    }
-			    
+			    data = new int[15][15];
+				    
+			}
+		});
+        
+        
+        startConnectionButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				/*try {
+					socket.close();
+				} catch (IOException e) {}
+				System.exit(0);*/
+				
+				
 				    
 			}
 		});
@@ -90,11 +149,11 @@ public class DataCreator2 extends JPanel implements MouseMotionListener, MouseLi
         super.paintComponent(g);
         
         g.setColor(Color.white);
-        g.fillRect(0, 0, 250, 250);
+        g.fillRect(0, 0, 150, 150);
         
         if(mouseDown){
         	g.setColor(Color.black);
-        	if(mouseX<250 && mouseY<250){
+        	if(mouseX<150 && mouseY<150 && mouseX>0 && mouseY>0){
         		int mX = (((int)mouseX)/10)*10;
         		int mY = (((int)mouseY)/10)*10;
         		
@@ -106,6 +165,7 @@ public class DataCreator2 extends JPanel implements MouseMotionListener, MouseLi
         }
         
         g.setColor(Color.black);
+        
         for(int i = 0; i<data.length;i++){
         	
         	for(int j = 0; j<data[i].length;j++){
@@ -113,6 +173,16 @@ public class DataCreator2 extends JPanel implements MouseMotionListener, MouseLi
             		g.fillRect(j*10, i*10, 10, 10);
             }
         }
+        
+        g.setColor(Color.red);
+        
+        for(int i = 0; i<info.length;i++){
+        	
+        	int value = info[i];
+        	g.fillRect(20, 160 + (i*20), value+5, 10);
+        	g.drawString(i+"", 10, 170 + (i*20));
+        }
+        
         
         //g.drawOval(0, 0, getWidth(), getHeight());
         
@@ -128,11 +198,70 @@ public class DataCreator2 extends JPanel implements MouseMotionListener, MouseLi
     	
         JFrame jFrame = new JFrame();
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setSize(265, 400);
+        jFrame.setSize(265, 450);
         jFrame.getContentPane().add(new DataCreator2());
         jFrame.setVisible(true);
     }
 
+    
+    Runnable mainRunnable = new Runnable() {
+		public void run() {
+			while(true){
+				try {
+					Thread.sleep(300);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				int index = 0;
+		    	for(int i =0; i<data.length;i++)
+		    		for(int j =0; j<data[i].length;j++)
+		    		{
+		    			byte d = (byte) data[i][j];
+		    			
+		    			writeData[index] = d;
+		    			index++;
+		    		}
+		    		
+		    	
+		    			
+		    	try {
+					out.write(writeData);
+					out.flush();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	
+		    	
+		    	try {
+		    		index = 0;
+		    		System.out.print("Packet Received: ");
+		    		int count = 0;
+		    		while(in.available()>0){
+		    			byte b = in.readByte();
+		    			
+		    			if(count%4 == 0){
+		    				
+		    				info[index] = b;
+		    				System.out.print(info[index]+" ");
+		    				index++;
+		    				
+		    			}
+		    			
+		    			count++;
+		    		}
+		    		System.out.print("\n");
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	
+			}
+		}
+	};
+	
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,73 +1,4 @@
 import numpy as np
-from PIL import Image
-
-#Neural network learns XOR
-#C:/Python34/Lib/site-packages/PyQt4/uic/pyuic.py nntest.ui -o nntest.py -x
-
-def XOR_TEST(): 
-    nn = NeuralNetwork([3,3,3,1],0.2,'tanh')
-    
-    question = [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
-    answer = [[0],[1],[1],[0],[1],[0],[0],[1]]
-
-    for i in range(0,5000):
-        random = np.random.randint(len(answer))
-        
-        questionToCopy = question[random]
-        questionCopied = []
-        
-        for x in range(0,len(questionToCopy)):
-            questionCopied.append(questionToCopy[x])
-
-        nn.feedtrain(questionCopied,answer[random])
-        
-    for i in range(0,len(answer)):
-        print(nn.feedforward(question[i]))
-
-#Neural network learns majority function.
-#If there are more 1's output 1 else output 0
-def MAJORITY_TEST():
-    nn = NeuralNetwork([9,5,5,1],0.2,'tanh')
-    
-    for i in range(0,5000):
-        newQuestion = []
-        nums1 = 0
-        nums0 = 0
-        ans = []
-        for y in range (0,9):
-            z = np.random.randint(2)
-            newQuestion.append(z)
-            if z == 0:
-                nums0 = nums0+1
-            else:
-                nums1 = nums1+1
-        if(nums1>nums0):
-            ans.append(1)
-        else:
-            ans.append(0)
-
-        nn.feedtrain(newQuestion,ans)
-
-    for i in range(0,10):
-        newQuestion = []
-        nums1 = 0
-        nums0 = 0
-        ans = []
-        for y in range (0,9):
-            z = np.random.randint(2)
-            newQuestion.append(z)
-            if z == 0:
-                nums0 = nums0+1
-            else:
-                nums1 = nums1+1
-        if(nums1>nums0):
-            ans.append(1)
-        else:
-            ans.append(0)
-        
-        print("Question: ",newQuestion)
-        print("Expected: ",ans)
-        print("Real: ",nn.feedforward(newQuestion))
 
 #Sigmoid activation of a given value
 def sigmoid(x):
@@ -102,14 +33,6 @@ class NeuralNetwork:
 
         self.weights = []
         
-        '''
-        for i in range(1, len(layers) - 1):
-            r = 2*np.random.random((layers[i-1] + 1, layers[i] + 1)) -1
-            self.weights.append(r)
-            
-        r = 2*np.random.random( (layers[i] + 1, layers[i+1] )) -1
-        '''
-
         if (tip == 0):
             for i in range(1, len(layers) - 1):
                 r = 2*np.random.random((layers[i-1], layers[i] )) -1
@@ -121,9 +44,10 @@ class NeuralNetwork:
             self.weights.append(r)
         else:
             self.weights = layers
+            
     #feed forward
     def feedforward(self, inputs):
-        inputs.append(1)
+       #inputs.append(1)
         result = []
         a = [inputs]
         
@@ -137,7 +61,7 @@ class NeuralNetwork:
 
     #back propagation traning 
     def feedtrain(self, inputs, expected):
-        inputs.append(1)
+        #inputs.append(1)
         a = [inputs]
         
         for l in range(len(self.weights)):
@@ -162,145 +86,6 @@ class NeuralNetwork:
     #return weights        
     def getweights(self):
         return self.weights
-           
-           
-##### Main Method #####
-
-if __name__ == '__main__':
-    
-    #nn = NeuralNetwork([225,225,112,56,25,10,10],0.2,'sigm')
-    inputNeurons = 225+1
-    layer = [inputNeurons,225,225,112,112,56,56,10,10]
-    nn = NeuralNetwork(layer,0.2,'sigm')
-
-    #layer = [4,3,2,1]
-    #nn = NeuralNetwork(layer,0.2,'sigm')
-    #filenumber = 0
-    
-    
-    for i in range(0,5000):
-        if(i%100 == 0):
-            print(i)
-
-        for x in range(0,10):
-            randSet = np.random.randint(30)
-            filenumber = np.random.randint(10)
-
-            random_file = 'TestData/'+str(randSet)+'_'+str(filenumber)+'.png'
-            im = Image.open(random_file)
-            data = list(im.getdata())
-            
-            question = []
-            
-            answer = [0,0,0,0,0,0,0,0,0,0]
-            answer[filenumber] = 1
-            
-            for j in range(0,225):
-                if(data[j][0] == 0):
-                    question.append(1)
-                else:
-                    question.append(0)
-            
-            nn.feedtrain(question,answer)
-
-            
-        
-        filenumber = filenumber+1
-        if filenumber == 10:
-            filenumber = 0
-        
-    for i in range(0,10):
-        for sets in range(0,41):
-            file = 'TestData/'+str(sets)+'_'+str(i)+'.png'
-            im = Image.open(file)
-            data = list(im.getdata())
-            question = []
-            for j in range(0,225):
-                if(data[j][0] == 0):
-                    question.append(1)
-                else:
-                    question.append(0)
-
-            ret = list(map(int,100*nn.feedforward(question)))
-                
-            whatItThought = 0
-            whatItThoughtIndex = 0
-                
-            for k in range(0,10):
-                if(ret[k]>whatItThought):
-                    whatItThought = ret[k]
-                    whatItThoughtIndex = k
-                  
-            print(i,": ",whatItThoughtIndex)
-        print("=====================")
-    
-    
-
-    
-   
-    weights = nn.getweights()
-    text_file = open("neuralnetwork_number_recognition.txt", "w")
-    text_file.write(str(len(layer))+'\n')
-    
-    for i in range(0,len(layer)):
-        text_file.write(str(layer[i])+'\n')
-        
-    for i in range(0,len(weights)):
-        w1 = weights[i]
-        #print('--------------')
-        for j in range(0,len(w1)):
-            w2 = w1[j]
-            #print('==============')
-            for k in range(0,len(w2)):
-                w3 = w2[k]
-                text_file.write(str(w3)+'\n')
-
-    text_file.close()
-    
-
-    
-    '''
-    details = []
-    weights = []
-    with open("neuralnetwork_number_recognition.txt") as f:
-        for line in f:  
-            numbers_str = line.split()
-            numbers_float = [float(x) for x in numbers_str] 
-            details.append(numbers_float[0])
-        
-    numberOfLayers = int(details[0])
-    layer = []
-    nextIndex = 1
-    for i in range(1,numberOfLayers+1):
-        layer.append(int(details[i]))
-        nextIndex = nextIndex +1
-    
-       
-    print(layer)
-    
-    for i in range(0,len(layer)-1):
-        firstlayer = layer[i]
-        secondlayer = layer[i+1]
-        arrayOutter = []
-        #print("====================")
-        for j in range(0,firstlayer):
-            #print("@@@@@@@@@@@@@@@@@")
-            arrayInner = []
-            for k in range(0,secondlayer):    
-                #print(details[nextIndex])
-                arrayInner.append(details[nextIndex])
-                nextIndex = nextIndex + 1
-            arrayOutter.append(arrayInner)
-        weights.append(arrayOutter)
-
-    
-    print(weights)
-    nn = NeuralNetwork(weights,0.2,'sigm',1)
-    print(nn.feedforward([5,2,-3]))
-    '''
-    #print(details)
-    #XOR_TEST()
-    #MAJORITY_TEST()
     
     
     
