cbd35ab6157dd63a120dc173cd53ec82c82aa92a
Inderpreet Pabla <inderpreet.pabla@gmail.com>
2016-08-23 22:26:57 -0400

    Easier client side socket connecting

--- PyAI/DataLoader.py
+++ PyAI/DataLoader.py
@@ -4,7 +4,8 @@ import os
 
 class DataLoader:
     
-    def __init__(self, data_paths = [], raw_data_filename = "raw_data.txt", size_x = 0, size_y = 0, num_inputs = 0, num_outputs = 0):
+    def __init__(self, data_paths = [], raw_data_filename = "raw_data.txt", 
+                 size_x = 0, size_y = 0, num_inputs = 0, num_outputs = 0):
         self.data_paths = data_paths
         self.raw_data_filename = raw_data_filename
         self.size_x = size_x
@@ -27,7 +28,6 @@ class DataLoader:
         else:
             paths = self.data_paths
         
-        
         for i in range(0, len(paths)):
             in_image,in_real,out_real = self.load_data(paths[i])
             self.input_images_set.append(in_image)
@@ -44,44 +44,8 @@ class DataLoader:
         # run through number of number of images to get
         for i in range(0,images_count):
             
-            file_name = path+str(i)+'.png' #create file name of get
-            stream = Image.open(file_name) #open file  in stream
-            
-            raw_image_data = list(stream.getdata()) #convert image file to a list
-            
-            raw_RGB = [[],[],[]] 
-            raw_count = 0
-            
-            # nested for loop for visual appeal, running (rex_x * rex_y) number of 
-            #times
-            for y in range(0,self.size_x):
-                
-                # extracting RGB, one row at a tile
-                red_row = []
-                green_row = []
-                blue_row = []
-            
-                #run through rows
-                for x in range(0,self.size_y):
-                    
-                    # get RGB values and scale them between 0.0 to 1.0
-                    red = (raw_image_data[raw_count][0]/255.0)
-                    green = (raw_image_data[raw_count][1]/255.0)
-                    blue = (raw_image_data[raw_count][2]/255.0)
-                              
-                    # append RGB to rows
-                    red_row.append(red)
-                    green_row.append(green)
-                    blue_row.append(blue)
-                    
-    
-                    #increment counter to move to the next data set of tuples
-                    raw_count = raw_count + 1 
-                
-                #append RGB rows to their corresponding location in raw RGB array
-                raw_RGB[0].append(red_row)
-                raw_RGB[1].append(green_row)
-                raw_RGB[2].append(blue_row)
+            file_path = path+str(i)+'.png' #create file name of get
+            raw_RGB = self.load_image(file_path)
               
             in_image.append(raw_RGB) # append broken down RGB array to images
             
@@ -116,11 +80,51 @@ class DataLoader:
         out_real = np.array(out_real, dtype = np.float32) 
         
         return in_image,in_real,out_real
+    
+    def load_image(self,image_path):
+        stream = Image.open(image_path) #open file  in stream
+            
+        raw_image_data = list(stream.getdata()) #convert image file to a list
+        
+        raw_RGB = [[],[],[]] 
+        raw_count = 0
+        
+        # nested for loop for visual appeal, running (rex_x * rex_y) number of 
+        #times
+        for y in range(0,self.size_x):
+            
+            # extracting RGB, one row at a tile
+            red_row = []
+            green_row = []
+            blue_row = []
+        
+            #run through rows
+            for x in range(0,self.size_y):
+                
+                # get RGB values and scale them between 0.0 to 1.0
+                red = (raw_image_data[raw_count][0]/255.0)
+                green = (raw_image_data[raw_count][1]/255.0)
+                blue = (raw_image_data[raw_count][2]/255.0)
+                          
+                # append RGB to rows
+                red_row.append(red)
+                green_row.append(green)
+                blue_row.append(blue)
+                
 
+                #increment counter to move to the next data set of tuples
+                raw_count = raw_count + 1 
+            
+            #append RGB rows to their corresponding location in raw RGB array
+            raw_RGB[0].append(red_row)
+            raw_RGB[1].append(green_row)
+            raw_RGB[2].append(blue_row)
+         
+        return raw_RGB
+         
     def get_set_elements_to_train(self,index):
         return [np.array(self.input_images_set[index]), 
-                np.array(self.input_reals_set[index])], 
-                np.array(self.output_reals_set[index])
+                np.array(self.input_reals_set[index])], np.array(self.output_reals_set[index])
                 
     def get_set_elements_to_predict(self,index):
         return [np.array(self.input_images_set[index]), 
--- PyAI/LaneDriver.py
+++ PyAI/LaneDriver.py
@@ -1,7 +1,7 @@
 import NetLoader as NetLoader
 import DataLoader as DataLoader
-
-
+import EasySocket as EasySocket
+import numpy as np
 
 
 if __name__ == "__main__":
@@ -11,16 +11,52 @@ if __name__ == "__main__":
     res_y = 50
     raw_input_size = 1
     raw_output_size = 3
-    
+    image_file = "../Tests/AdvancedCarModel/real_time.png"
     image_val_data_location_1 = "../Tests/AdvancedCarModel/Data18/"
-
+    
+    '''
+    socket = EasySocket.EasySocket(preset_unpack_types = ['fff'])
+    b = socket.get_array_data(1,0)
+    print(np.array(b))
+    '''
     
     net = NetLoader.NetLoader(model_file=model_path,weights_file=weights_path)
 
     data = DataLoader.DataLoader([image_val_data_location_1], size_x = res_x,
                                  size_y=res_y, num_inputs=raw_input_size, 
                                  num_outputs=raw_output_size)
-    
+    '''
     input_element,output_element = data.get_set_elements_to_train(0)
     net.fit(input_element,output_element,verbose = 2)
+    '''
     
+    socket = EasySocket.EasySocket(preset_unpack_types = ['ff'])
+    socket.connect()
+    while True:
+        get_data = np.array(socket.get_array_data(8,0))
+        real_data = np.array([get_data[1]],dtype=np.float32)
+        raw_RGB = data.load_image(image_file)
+        raw_RGB = np.array(raw_RGB,dtype = np.float32)
+        pre = net.predict([np.array([raw_RGB]),np.array([real_data])])
+        
+        pre = pre[0]
+
+         
+        max_num = 0
+        prediction_answer = 0
+        for m in range(len(pre)):
+            if pre[m] > max_num:
+                max_num = pre[m]
+                prediction_answer = m
+        
+        if(prediction_answer == 0): 
+            prediction_answer = -1
+        elif(prediction_answer == 1): 
+            prediction_answer = 0
+        else:
+            prediction_answer = 1
+           
+        message = str(prediction_answer)
+        
+        socket.send_string_data(message)
+    socket.close()
