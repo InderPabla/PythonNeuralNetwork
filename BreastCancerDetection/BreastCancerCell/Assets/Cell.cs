5db37d8ccdc9d59974a4f8b4fcef9aea06d52395
Inderpreet Pabla <inderpreet.pabla@gmail.com>
2016-11-16 23:51:25 -0500

    Cancer cell blandness

--- BreastCancerDetection/BreastCancerCell/Assets/Cell.cs
+++ BreastCancerDetection/BreastCancerCell/Assets/Cell.cs
@@ -6,12 +6,12 @@ public class Cell : MonoBehaviour {
     public GameObject cellPrefab;
 
     float thickness = 5; 
-    float size = 10;
+    float size = 2;
     float shape = 1;
-    float adhesion = 2;
+    float adhesion = 10;
     float epi = 0;
     float bare = 10;
-    float bland = 0;
+    float bland = 1;
     float normal = 0;
     float mitoses = 10;
 
@@ -35,30 +35,30 @@ public class Cell : MonoBehaviour {
         float height = UnityEngine.Random.Range(0.5f, 0.5f + shapeFactor);
 
         float sizeFactor = size /15f;
-        float nucleasSizeFactor = bare / 15f;
+        float nucleasSizeFactor = bare / 2f;
 
         float minSize = 0f;
         if (width > height)
         {
-            
-            transform.localScale = new Vector3(0.5f * (sizeFactor + 1f), 0.5f*(height/width) * (sizeFactor + 1f), 0.01f);
+
+            float sizeX = 0.5f * (sizeFactor + 1f);
+            float sizeY = 0.5f * (height / width) * (sizeFactor + 1f);
+
+            transform.localScale = new Vector3(UnityEngine.Random.Range(0.5f,sizeX), UnityEngine.Random.Range(0.5f, sizeY), 0.01f);
             minSize = height;
-
-            /*transform.GetComponent<CircleCollider2D>().radius = 0.533f * (height / width)
-                (0.533f - (0.1f * ((thickness / 10f)))) * (height/width) * (sizeFactor + 1f);*/
-
             transform.GetComponent<CircleCollider2D>().radius = (0.533f - (0.1f * (thickness / 10f))) * (height / width);
 
             
         }
         else
         {
+            float sizeX = 0.5f * (width / height) * (sizeFactor + 1f);
+            float sizeY = 0.5f * (sizeFactor + 1f);
+
+            transform.localScale = new Vector3(UnityEngine.Random.Range(0.5f, sizeX), UnityEngine.Random.Range(0.5f, sizeY), 0.01f);
+
             
-            transform.localScale = new Vector3(0.5f * (width/height) * (sizeFactor + 1f), 0.5f * (sizeFactor + 1f), 0.01f);
             minSize = width;
-
-            /*transform.GetComponent<CircleCollider2D>().radius = (0.533f-(0.1f*((thickness/10f)))) * (width / height) *(sizeFactor + 1f);*/
-
             transform.GetComponent<CircleCollider2D>().radius = (0.533f -(0.1f*(thickness/10f))) * (width/height);
         }
 
@@ -68,6 +68,18 @@ public class Cell : MonoBehaviour {
         float newYSize =  transform.localScale.y / fixedRatio;
         Debug.Log(newXSize + " " + fixedRatio + " " + transform.localScale.x);
         transform.GetChild(0).localScale = new Vector3(newXSize * (nucleasSizeFactor + 1f), newYSize * (nucleasSizeFactor + 1f), 0.01f);
+
+        float highestAlpha = 0.75f;
+        float highestRemoveAlpha = 0.5f;
+        float alphaRatio = bland / 10f;
+        float newAlpha = highestRemoveAlpha * (1f - alphaRatio);
+        Color color = Color.red;
+
+        color.a = UnityEngine.Random.Range(0f, highestAlpha- highestRemoveAlpha);
+        
+        transform.GetChild(1).GetComponent<Renderer>().material.color = color;
+        transform.GetChild(1).GetComponent<Renderer>().material.shader = Shader.Find("Transparent/Diffuse");
+
 
         if (mitoses > 0)
         {
